9f92d5b74802f23ec0563f98fd569afe6cc47b70
yuooo <j3ss0u@gmail.com>
2016-04-24 20:01:22 -0500

    ++ Jars, Features package

--- src/com/mess/Features/Features.java
+++ src/com/mess/Features/Features.java
@@ -1,8 +1,12 @@
 package com.mess.Features;
 
+import weka.core.Instance;
+
 /**
  * Created by jessicahoffmann on 24/04/2016.
  */
 public abstract class Features {
+
+    public abstract Instance toWeka();
 
 }
