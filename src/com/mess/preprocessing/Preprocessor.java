b1d7cd7c6c6d0a661172d22e211111ce88cf5a63
mdenend <mdenend@gmail.com>
2016-04-24 20:18:28 -0500

    ++ Preprocessor class (with TODOs), jars for stanford parser and pdf to text converter

---
+++ src/com/mess/preprocessing/Preprocessor.java
@@ -0,0 +1,14 @@
+package com.mess.preprocessing;
+
+/**
+ * Created by mad4672 on 4/24/16.
+ */
+public class Preprocessor {
+    /**
+     * How to build this:
+     * 1. Take a PDF file (have it be located in PDFFiles/) and convert the entirety of that file into a text file. Use this tool: https://pdfbox.apache.org/
+     * 2. Save this text file into a new document. We'll put this file into TextFiles/.
+     * 2. Using this text file, create a parse tree file as well, using the default Stanford Parser We'll put this file into .
+     */
+
+}
