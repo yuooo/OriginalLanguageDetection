a50fd620fb21f921b1a4d0464782869af3f561ea
yuooo <j3ss0u@gmail.com>
2016-04-24 21:18:26 -0500

    ++ 3 Features classes

---
+++ src/com/mess/Features/LexicalFeature.java
@@ -0,0 +1,15 @@
+package com.mess.Features;
+
+import weka.core.Instance;
+
+/**
+ * Created by jessicahoffmann on 24/04/2016.
+ */
+public class LexicalFeature extends Features {
+
+    // TODO
+    @Override
+    public Instance toWeka() {
+        return null;
+    }
+}
---
+++ src/com/mess/Features/ParseFeature.java
@@ -0,0 +1,14 @@
+package com.mess.Features;
+
+import weka.core.Instance;
+
+/**
+ * Created by jessicahoffmann on 24/04/2016.
+ */
+public class ParseFeature extends Features {
+    // TODO
+    @Override
+    public Instance toWeka() {
+        return null;
+    }
+}
