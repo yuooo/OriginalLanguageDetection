3a108d6f1c5ec7c264a6980afddb8bd119eb185e
yuooo <j3ss0u@gmail.com>
2016-04-27 16:21:41 -0500

    ++ compute unigram (Lexical Features)

--- src/main/java/mess/Features/LexicalFeature.java
+++ src/main/java/mess/Features/LexicalFeature.java
@@ -1,17 +1,22 @@
 package mess.Features;
 
 import weka.core.Instances;
+import weka.core.converters.TextDirectoryLoader;
 import weka.core.tokenizers.NGramTokenizer;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
+
+import java.io.File;
 
 /**
  * Created by jessicahoffmann on 24/04/2016.
  */
 public class LexicalFeature extends Features {
-    // TODO
-    public Instances computeUnigram(String pathFile) {
+    public Instances computeUnigram(String pathFile) throws Exception {
         // load the data
+        TextDirectoryLoader txtLoader = new TextDirectoryLoader();
+        txtLoader.setDirectory(new File(pathFile));
+        Instances inputInstances = txtLoader.getDataSet();
 
         // Set the tokenizer
         NGramTokenizer tokenizer = new NGramTokenizer();
@@ -28,8 +33,8 @@ public class LexicalFeature extends Features {
         filter.setLowerCaseTokens(true);
 
         // Filter the input instances into the output ones
-        outputInstances = Filter.useFilter(inputInstances,filter);
-        return null;
+        Instances outputInstances = Filter.useFilter(inputInstances,filter);
+        return outputInstances;
     }
 
     // TODO
