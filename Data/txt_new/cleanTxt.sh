9f5784386cc47944a31b5e8cac8a0e2303151a5b
mdenend <mdenend@gmail.com>
2016-05-06 17:56:55 -0500

    Changed cleanTxt.sh

--- Data/txt_new/cleanTxt.sh
+++ Data/txt_new/cleanTxt.sh
@@ -1,9 +1,20 @@
+#Variations:
+#Do this PER NOVEL. Not per directory.
+
+#Roman numerals: [IVX]+\.
+
+#Remove subtitle after chapter:
+#CHAPTER
+
+#^[A-Z]
+
+
 #!/bin/bash
 for file in $1/*
 do
     filename_out="${file%.*}"
-    file_out="../txt_clean/${filename_out}_clean.txt"
+    file_out="../txt_clean_new/${filename_out}_clean.txt"
     echo $file_out 
-    gsed -e "/^CHAPTER\|^BOOK\|^VOLUME/Id" -e "/^PART/d" $file > $file_out
+    sed -e "/^CHAPTER\|^BOOK\|^VOLUME/Id" -e "/^PART/d" $file > $file_out
 done
 
