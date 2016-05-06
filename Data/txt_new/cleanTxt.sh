e7175881c3571f873538238d7639b7bf7a3c736e
yuooo <j3ss0u@gmail.com>
2016-05-06 17:54:57 -0500

    correct Data/ structure

--- Data/txt_new/cleanTxt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in $1/*
-do
-    filename_out="${file%.*}"
-    file_out="../txt_clean/${filename_out}_clean.txt"
-    echo $file_out 
-    gsed -e "/^CHAPTER\|^BOOK\|^VOLUME/Id" -e "/^PART/d" $file > $file_out
-done
-
