4bea192ef7ac5b0e9dcd94077a63bf7a9a7a4080
mdenend <mdenend@gmail.com>
2016-05-11 04:13:34 -0500

    I've done as much work as I could muster. I've got HomemadeFeature and ParseFeature basically all written, however they have not been tested yet. I've also created Instances for HomemadeFeatures, but not for ParseFeatures yet. Still a lot of work to be done tomorrow but I'm very close.

--- src/main/java/mess/Features/HomemadeFeature.java
+++ src/main/java/mess/Features/HomemadeFeature.java
@@ -1,12 +1,12 @@
 package mess.Features;
 
 import mess.utils.POS;
+import mess.utils.TextPOSTreeTriple;
 import mess.utils.TreeToSentenceHandler;
+import weka.core.Attribute;
 
 import java.io.File;
-import java.util.EnumMap;
-import java.util.EnumSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by jessicahoffmann on 24/04/2016.
@@ -116,29 +116,94 @@ public class HomemadeFeature extends Features {
     //TODO: and the values are those in which we increment. Also, a mapping for each count would be good.
     //TODO: Make each mapping a special Object, or just a String? Hmm...
 
-    private EnumMap<POS, Set<String>> map;
+    //private EnumMap<POS, Set<String>> map;
+    private HomemadeFeatureVector v;
+    private Set<String> wordType;
+
 
     public HomemadeFeature() {
-        map = new EnumMap<POS, Set<String>>(POS.class);
-        for (POS p : map.keySet()) {
+        //map = new EnumMap<POS, Set<String>>(POS.class);
+        //for (POS p : map.keySet()) {
 
-        }
+        //}
+        HomemadeFeatureVector v = new HomemadeFeatureVector();
+        wordType = new HashSet<>();
+
     }
+//File file = new File ("Data/blocks/500/train/American/The_Adventures_of_Huckleberry_Finn_clean_00001.txt");
+    //TreeToSentenceHandler t = new TreeToSentenceHandler();
+    //Massive Mapping: File to HomemadeFeatureVector.
+    // FeatureCounterFactory (FeatureCounterFactory takes in Features you're trying to compute and creates FeatureCounters. Has a
+    //bunch of numbers, then we compute the ratios that we need at the very end and put them into HomemadeFeatureVector.
+    //For each file
+    //Create a TreeToSentenceHandler
+    // For each TextPOSTreeTriple we can get from TreeToSentenceHandler
+    //First, do the clause parsing... by using a BFS.
+    //Here, If a sentence has more than one clause, complex. Otherwise, simple.
+    //ALSO... If there is an IN inside a PP, DECREMENT conjunctions.
 
-    public void computeHomemadeFeatures () {
-        File file = new File ("Data/blocks/500/train/American/The_Adventures_of_Huckleberry_Finn_clean_00001.txt");
-        //TreeToSentenceHandler t = new TreeToSentenceHandler();
-        //Massive Mapping: File to HomemadeFeatureVector.
+    public void computeHomemadeFeatures (TextPOSTreeTriple t, int simpleSentence, int complexSentence, int prepositions, int conjunctions) {
+
+        List<String> words = t.getWords();
+        List<String> POStags = t.getPOS();
+        for (int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+            if (word.length() == 1) {
+                System.out.print(word + " ");
+            }
+
+            //kinda ugly, but hey... think this is the cleanest way to prevent punctuation errors.
+            if (word.matches("([\\.\\?!;:\\-\\(\\)\\[\\]]|['`]{2})")){
+                continue;
+            }
+            String POSStr = POStags.get(i);
+            List<String> features = POS.valueOf(POSStr).getHomemadeFeatures();
+            //increment ALL of the features
+            for (String feat : features) {
+                switch (feat) {
+                    case "Word":
+                        v.incrementFeature(HomemadeFeatureCounterMapping.TotalWords);
+                        wordType.add(word);
+                        break;
+                    case "FV":
+                        v.incrementFeature(HomemadeFeatureCounterMapping.FiniteVerbs);
+                        break;
+                    case "Num":
+                        v.incrementFeature(HomemadeFeatureCounterMapping.Numerals);
+                        break;
+                    case "Conjuncts":
+                        v.incrementFeature(HomemadeFeatureCounterMapping.Conjunctions);
+                        break;
+                    case "Noun":
+                        v.incrementFeature(HomemadeFeatureCounterMapping.Nouns);
+                        break;
+                    case "Pron":
+                        v.incrementFeature(HomemadeFeatureCounterMapping.Pronouns);
+                        break;
+                    case "Closed": //case closed! hahahahaha
+                        v.incrementFeature(HomemadeFeatureCounterMapping.ClosedClassWords);
+                        break;
+                    case "Open":
+                        v.incrementFeature(HomemadeFeatureCounterMapping.OpenClassWords);
+                        break;
+                    case "Neither":
+                        break;
+                    default:
+                        System.err.println("Unexpected feature!!! " + feat);
+                }
+            }
+            //character count
+            v.addToFeature(HomemadeFeatureCounterMapping.TotalCharacters, word.length());
+        }
+
+        //these came from the ParsingFeatures
+        v.addToFeature(HomemadeFeatureCounterMapping.SimpleSentences, simpleSentence);
+        v.addToFeature(HomemadeFeatureCounterMapping.ComplexSentences, complexSentence);
+        v.addToFeature(HomemadeFeatureCounterMapping.Prepositions, prepositions);
+        v.addToFeature(HomemadeFeatureCounterMapping.Conjunctions, conjunctions);
 
 
-        // FeatureCounterFactory (FeatureCounterFactory takes in Features you're trying to compute and creates FeatureCounters. Has a
-                //bunch of numbers, then we compute the ratios that we need at the very end and put them into HomemadeFeatureVector.
-        //For each file
-            //Create a TreeToSentenceHandler
-            // For each TextPOSTreeTriple we can get from TreeToSentenceHandler
-                //First, do the clause parsing... by using a BFS.
-                    //Here, If a sentence has more than one clause, complex. Otherwise, simple.
-                    //ALSO... If there is an IN inside a PP, DECREMENT conjunctions.
+
                 //Then, grab the List created from the enum of homemade feature types.
                 //Set lastWord to "".
                 //For each word in this list:
@@ -151,9 +216,156 @@ public class HomemadeFeature extends Features {
 
     }
 
-    private class FeatureCounter {
+
+    //Done counting, now I compute percentages.
+    public EnumMap<HomemadeFeatureRatioNames, Double> computePercentages() {
+        //first, compute the wordTypes for the ENTIRE directory.
+        int wordTypes = wordType.size();
+        v.addToFeature(HomemadeFeatureCounterMapping.WordTypes, wordTypes);
+
+        EnumMap<HomemadeFeatureRatioNames, Double> m = new EnumMap<>(HomemadeFeatureRatioNames.class);
+        EnumMap<HomemadeFeatureCounterMapping, Integer> counts = v.gethomemadeAttributes();
+        for (HomemadeFeatureRatioNames r : HomemadeFeatureRatioNames.values()) {
+            switch (r) {
+                case simplecomplex:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.SimpleSentences)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.ComplexSentences));
+                    break;
+                case simpletotal:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.SimpleSentences)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalSentences));
+                    break;
+                case complextotal:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.ComplexSentences)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalSentences));
+                    break;
+                case avgsent:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.TotalWords)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalSentences));
+                    break;
+                case avgwordlength:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.TotalCharacters)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case fverbratio:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.FiniteVerbs)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case numratio:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.Numerals)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case prepratio:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.Prepositions)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case conjratio:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.Conjunctions)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case infoload:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.OpenClassWords)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case nounratio:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.FiniteVerbs)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case grammlex:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.OpenClassWords)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.ClosedClassWords));
+                    break;
+                case pronounratio:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.Pronouns)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+                case typetoken:
+                    m.put(r, (double) counts.get(HomemadeFeatureCounterMapping.WordTypes)
+                            /(double) counts.get(HomemadeFeatureCounterMapping.TotalWords));
+                    break;
+            }
+        }
+        return m;
 
     }
 
+
+    protected class HomemadeFeatureVector {
+
+
+        private EnumMap<HomemadeFeatureCounterMapping, Integer> homemadeAttributes;
+
+        HomemadeFeatureVector() {
+            homemadeAttributes = new EnumMap<>(HomemadeFeatureCounterMapping.class);
+            for (HomemadeFeatureCounterMapping f : HomemadeFeatureCounterMapping.values()) {
+                homemadeAttributes.put(f, 0);
+            }
+        }
+
+        void incrementFeature(HomemadeFeatureCounterMapping m) {
+            int i = homemadeAttributes.get(m);
+            i++;
+            homemadeAttributes.put(m,i);
+        }
+
+        void addToFeature (HomemadeFeatureCounterMapping m, int num) {
+            int i = homemadeAttributes.get(m);
+            i+=num;
+            homemadeAttributes.put(m,i);
+        }
+
+        EnumMap<HomemadeFeatureCounterMapping, Integer> gethomemadeAttributes () {
+            return homemadeAttributes;
+        }
+
+    }
+
+    public enum HomemadeFeatureCounterMapping {
+        SimpleSentences(0),
+        ComplexSentences(1),
+        TotalSentences(2),
+        TotalWords(3),
+        TotalCharacters(4),
+        FiniteVerbs(5),
+        Numerals(6),
+        Prepositions(7),
+        Conjunctions(8),
+        OpenClassWords(9),
+        Nouns(10),
+        ClosedClassWords(11),
+        Pronouns(12),
+        WordTypes(13);
+
+        private int index;
+
+        HomemadeFeatureCounterMapping (int index) {
+            this.index = index;
+        }
+    }
+
+    public enum HomemadeFeatureRatioNames {
+        simplecomplex,
+        simpletotal,
+        complextotal,
+        avgsent,
+        avgwordlength,
+        fverbratio,
+        numratio,
+        prepratio,
+        conjratio,
+        infoload,
+        nounratio,
+        grammlex,
+        pronounratio,
+        typetoken;
+
+        public static int size() {
+            int i = 0;
+            for (HomemadeFeatureRatioNames h : HomemadeFeatureRatioNames.values()) {
+                i++;
+            }
+            return i;
+        }
+    }
 
 }
--- src/main/java/mess/Features/ParseFeature.java
+++ src/main/java/mess/Features/ParseFeature.java
@@ -11,11 +11,19 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.Treebank;
 import edu.stanford.nlp.util.Pair;
 import edu.stanford.nlp.util.Timing;
+import mess.Features.HomemadeFeature;
+import mess.utils.TextPOSTreeTriple;
+import mess.utils.TreeToSentenceHandler;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.lang.reflect.Array;
 import java.util.*;
+
 
 /**
  * Created by jessicahoffmann on 24/04/2016.
@@ -34,13 +42,25 @@ public class ParseFeature extends Features {
      * e.g.: java ParserDemo edu/stanford/nlp/models/lexparser/chineseFactored.ser.gz data/chinese-onesent-utf8.txt
      */
 
-    public HashMap<String, Integer[]> featuresCounts;
+    //public HashMap<String, Integer[]> featuresCounts;
+    private final Set<String> clauses;
 
 
     public ParseFeature() {
-
+        List<String> temp = Arrays.asList("S",
+        "SBAR",
+        "SBARQ",
+        "SINV",
+        "SQ");
+        clauses = new HashSet<>(temp);
     }
 
+    /**
+     * Matt Changes here:
+     * I decided it would make the most sense to go ahead and compute the HomemadeFeatures based off of the BFS being done here.
+     * I changed from creating a Treebank to using my built in Treebank method since I also grab the words and POS, which I rely upon
+     * heavily in Homemade Features.
+     */
     public void parseMe() {
         Options op = new Options();
         op.doDep = false;
@@ -49,55 +69,112 @@ public class ParseFeature extends Features {
         File directory = new File("Data/parseFeatureTesting/train");
         File[] files = directory.listFiles();
         Integer n = files.length;
+        List<HashMap<String, List<Integer>>> sliceMaps = new ArrayList<>();
+        List<EnumMap<HomemadeFeature.HomemadeFeatureRatioNames, Double>> homemadeMaps = new ArrayList<>();
         for (int j = 0; j < n; j++) {
 
-            Treebank langTreeBank = makeTreebankie(files[j].toString(), op, null);
+            sliceMaps.add(new HashMap<>());
+            homemadeMaps.add(new EnumMap<>(HomemadeFeature.HomemadeFeatureRatioNames.class));
+            //Changed by Matt: Using my function instead since this will make Homemade Computation much easier.
+            //Treebank langTreeBank = makeTreebankie(files[j].toString(), op, null);
+            HashMap<String, List<Integer>> featuresCounts = sliceMaps.get(j);
+            HomemadeFeature hm = new HomemadeFeature();
+            File[] subDirectories  = files[j].listFiles();
+            //featuresCounts = new HashMap<>();
+            for (int k = 0; k < subDirectories.length; k++) {
+                TreeToSentenceHandler ite = new TreeToSentenceHandler(subDirectories[k]);
+                List<Tree> children;
+                Tree t1;
 
-            featuresCounts = new HashMap<>();
-            Iterator<Tree> ite = langTreeBank.iterator();
-            List<Tree> children;
-//            Iterator<Tree> ;
-            Tree t1;
+                while (ite.hasNext()) {
+                    int simpleSentence = 0, complexSentence = 0, prepositions = 0, conjunctions = 0;
+                    Queue<Tree> treeNodes = new LinkedList<Tree>();
+                    TextPOSTreeTriple trip = ite.generateSentence();
+                    t1 = trip.getTree();
+                    treeNodes.add(t1.firstChild());
+                    int clauseCount = 0; //num of clauses in a sentence
 
-            while (ite.hasNext()) {
-                Queue<Tree> treeNodes = new LinkedList<Tree>();
-                t1 = ite.next();
-                treeNodes.add(t1.firstChild());
-                while (!treeNodes.isEmpty()) {
-                    Tree tNode = treeNodes.remove();
-                    children = tNode.getChildrenAsList();
-                    System.out.println(tNode.nodeString());
-                    System.out.println("Children: ");
-                    String key = tNode.nodeString();
-                    for (Tree child : children) {
-                        System.out.print(child.nodeString() + " ");
-                        if (child.isLeaf() || child.firstChild().isLeaf()) {
-                            continue;
+                    while (!treeNodes.isEmpty()) {
+                        boolean prepPhraseFlag = false; //if true, we decrement conjunction count to not double count it.
+                        Tree tNode = treeNodes.remove();
+                        children = tNode.getChildrenAsList();
+                        System.out.println(tNode.nodeString());
+                        System.out.println("Children: ");
+                        String key = tNode.nodeString();
+                        //clause check!
+                        if (clauses.contains(key)) {
+                            clauseCount++;
+                        //prepositional phrase check!
+                        } else if (key.equals("PP")) {
+                            prepositions++;
+                            prepPhraseFlag = true;
                         }
-                        treeNodes.add(child);
-                        //System.out.println(child1.firstChild().nodeString());
-                        //System.out.println(child1.firstChild().firstChild().nodeString());
-                        key = key + "_" + child.nodeString();
+                        for (Tree child : children) {
+                            System.out.print(child.nodeString() + " ");
+                            if (prepPhraseFlag && child.nodeString().equals("IN")) {
+                                conjunctions--;
+                            }
+                            if (child.isLeaf() || child.firstChild().isLeaf()) {
+                                continue;
+                            }
+                            treeNodes.add(child);
+                            //System.out.println(child1.firstChild().nodeString());
+                            //System.out.println(child1.firstChild().firstChild().nodeString());
+                            key = key + "_" + child.nodeString();
+                        }
+                        System.out.println();
+                        System.out.println(key);
+                        System.out.println("");
+                        if (featuresCounts.containsKey(key)) {
+                            List<Integer> arr = featuresCounts.get(key);
+                            int val = arr.get(j);
+                            val++;
+                            arr.add(j,val);
+                            featuresCounts.put(key, arr);
+                        }
+                        else {
+                            //Integer[] arr = Collections.nCopies(n, 0).toArray(new Integer[0]); ??????
+                            List<Integer> arr = new ArrayList<>();
+                            //arr[j]++;
+                            arr.add(j, 1);
+                            featuresCounts.put(key, arr);
+                        }
                     }
-                    System.out.println();
-                    System.out.println(key);
-                    System.out.println("");
-                    if (featuresCounts.containsKey(key)) {
-                        Integer[] arr = featuresCounts.get(key);
-                        arr[j]++;
-                        featuresCounts.put(key, arr);
-                    }
-                    else {
-                        Integer[] arr = Collections.nCopies(n, 0).toArray(new Integer[0]);
-                        arr[j]++;
-                        featuresCounts.put(key, arr);
-                    }
+                    if (clauseCount > 1)
+                        complexSentence++;
+                    else
+                        simpleSentence++;
+
+                    //Compute the homemade features here!
+                    hm.computeHomemadeFeatures(trip, simpleSentence, complexSentence, prepositions, conjunctions);
                 }
+
+
             }
-
-
+            //Now crunch all of the counts for the HomemadeFeatures and the ParseFeatures.
+            EnumMap<HomemadeFeature.HomemadeFeatureRatioNames, Double> ratios = hm.computePercentages();
+            homemadeMaps.add(ratios);
             System.out.println(featuresCounts.toString());
         }
+
+        //Compute Homemade Instances
+        Set<HomemadeFeature.HomemadeFeatureRatioNames> s = new TreeSet<>();
+        for (EnumMap<HomemadeFeature.HomemadeFeatureRatioNames, Double> m : homemadeMaps) {
+            s.addAll(m.keySet());
+        }
+        ArrayList<Attribute> list = new ArrayList<>();
+        for (HomemadeFeature.HomemadeFeatureRatioNames f : s) {
+            Attribute a = new Attribute(f.toString());
+            list.add(a);
+        }
+        Instances homemadeIns = new Instances("homemade", list, homemadeMaps.size());
+        for (int i = 0; i < homemadeMaps.size(); i++) {
+            Instance inst = new DenseInstance(HomemadeFeature.HomemadeFeatureRatioNames.size());
+            for (Attribute a : list) {
+                inst.setValue(a, homemadeMaps.get(i).get(HomemadeFeature.HomemadeFeatureRatioNames.valueOf(a.name())));
+            }
+        }
+        //TODO: Compute the instances for slices, and return these?.
     }
 
     public static Treebank makeTreebankie(String treebankPath, Options op, FileFilter filt) {
